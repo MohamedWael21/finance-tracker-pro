a5743a0a3b529bcfb6853ee0ed3fff481621cd2f
Mohamed Wael <mohame171819@gmail.com>
2026-05-01 19:21:59 +0300

    finish badge component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { Tabs } from './shared/components/ui/tabs/tabs';
-import { TabPanel } from './shared/components/ui/tabs/tab-panel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Tabs, TabPanel],
 })
 export class App {
   protected readonly title = signal('finance-tracker-pro');
