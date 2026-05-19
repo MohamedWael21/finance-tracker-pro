1919eb60a0b3a12e2fd2f92687347c59dae88cfd
HONDA-74 <mohanadtarek742203@gmail.com>
2026-05-19 23:39:09 +0300

    fix: update chatbot service endpoint to use rawURL from environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  rawURL : 'https://finance-tracker-app-ochre.vercel.app',
   baseURL: 'https://finance-tracker-app-ochre.vercel.app/api/v1',
   notification: 'https://finance-tracker-app-ochre.vercel.app/api/v1/notifications',
   user: 'https://finance-tracker-app-ochre.vercel.app/api/v1/user',
