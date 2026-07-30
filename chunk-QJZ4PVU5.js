e79b0885d132bd861e4e21c3f46b60f5480f6367
Mohamed Wael <mohame171819@gmail.com>
2026-07-30 03:02:20 +0300

    Auto-generated commit

---
+++ chunk-QJZ4PVU5.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var o=[{path:"",loadComponent:()=>import("./chunk-NQZHKKVC.js").then(t=>t.BudgetList)}];export{o as BUDGET_ROUTES};
---
+++ chunk-QND3CLUN.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:()=>import("./chunk-3WDWVV32.js").then(o=>o.CategoryList)}];export{t as CATEGORY_ROUTES};
